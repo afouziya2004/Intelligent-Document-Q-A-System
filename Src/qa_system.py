8f6bae9f24d3b3385b68c3ca023d6cab8c7af10e
Fouziya <afouziya2004@gmail.com>
2026-04-12 16:58:26 +0530

    Create qa_system.py

---
+++ Src/qa_system.py
@@ -0,0 +1,30 @@
+from loader import load_document, split_text
+from embedder import create_embeddings
+from retriever import retrieve
+
+def run_qa_system():
+    print("🔄 Loading document...")
+
+    text = load_document("data/sample.txt")
+    chunks = split_text(text)
+
+    vectorizer, vectors = create_embeddings(chunks)
+
+    print("✅ System Ready! Ask questions (type 'exit' to stop)\n")
+
+    while True:
+        query = input("❓ Your Question: ")
+
+        if query.lower() == "exit":
+            print("👋 Exiting system...")
+            break
+
+        result, score = retrieve(query, vectorizer, vectors, chunks)
+
+        print("\n💡 Answer:")
+        print(result)
+        print(f"\n🔎 Similarity Score: {score:.4f}\n")
+
+
+if __name__ == "__main__":
+    run_qa_system()
